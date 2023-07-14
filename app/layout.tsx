a29103780b50ef978a604a8b6938ff0fd71d9bff
str4ng3love <normalemailadress@gmail.com>
2023-07-14 10:23:41 +0200

    changed main font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,9 @@
 import "./globals.css";
-import { Abel } from "next/font/google";
 import Script from "next/script";
 export const metadata = {
-  title: "",
-  description: "",
-  icons: {
-    icon: './images/favicon.ico' 
-  }
+
 };
-const abel = Abel({
-  subsets: ["latin"],
-  variable: "--abel-font",
-  weight: "400",
-});
+
 
 export default function RootLayout({
   children,
@@ -22,7 +13,7 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
-        <body className={abel.variable}>{children}</body>
+        <body>{children}</body>
         <Script src="./scripts/theme.js" />
       </html>
     </>
