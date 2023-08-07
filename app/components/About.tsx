53b1912f5ee4361aad1fc7d95d62c53590d2213c
str4ng3love <normalemailadress@gmail.com>
2023-08-07 11:29:18 +0200

    added some text to `about` component

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -31,7 +31,7 @@ const About = () => {
       id="about"
       className="bg-fixed group mb-16 mt-32 text-light-color bg-dark-color dark:text-dark-color dark:bg-light-a80 relative flex justify-center dark:shadow-link-dark-fat shadow-inner bg-gradient-grid dark:bg-gradient-grid-dark bg-3x3 animate-gridMove dark:animate-gridMoveDark transition-all 500ms delay-75"
     >
-      <div className="flex flex-col items-center p-4 sm:p-8 pt-16 pb-16 backdrop-blur-sm bg-transparent w-full dark:hover:backdrop-saturate-100 hover:backdrop-saturate-200 transition-all 300ms">
+      <div className="flex flex-col items-center p-4 sm:p-8 pt-16 pb-16 backdrop-blur-sm bg-transparent w-full dark:hover:backdrop-saturate-200 hover:backdrop-saturate-200 transition-all 300ms">
         <div
           className="rounded-md overflow-clip flex flex-col items-center dark:bg-light-color dark:shadow-link-dark-fat bg-dark-color shadow-link-light-fat p-4 sm:p-8 w-full lg:w-[70%] md:w-[80%] sm:w-[90%] ring-inset ring-2 ring-violet-a40
         "
@@ -39,15 +39,40 @@ const About = () => {
           <h3 className="text-2xl font-bold first-letter:capitalize text-center p-8 sm:mt-12">
             Experience
           </h3>
-          <p className="p-8 sm:w-[80%] w-full text-center text-lg">
+          <p className="p-8 sm:w-[80%] w-full text-center text-lg semibol">
             While working on many personal projects, I learned how to design,
             build and ship front-end and fullstack web apps. Solid fundamentals,
             fast learning and perseverance allow me to tackle any project.
           </p>
           {showText ? (
-            <div className="sm:w-[80%] w-full text-center text-lg">
-              
-            </div>
+            <>
+              <p className="sm:w-[80%] w-full text-center text-lg p-8">
+                My time with React/Next ecosystem taught me how to use:
+              </p>
+              <div className="flex sm:flex-row flex-col justify-center text-sm sm:text-base">
+                <ul className="flex flex-col items-start sm:p-8 list-inside">
+                  <li className="list-disc p-2 ">
+                    Functional Components and Hooks
+                  </li>
+                  <li className="list-disc p-2 ">Promises</li>
+                  <li className="list-disc p-2 ">Server Side Rendering</li>
+                  <li className="list-disc p-2 ">Create and Consume APIs</li>
+                </ul>
+                <ul className="flex flex-col items-start  sm:p-8 list-inside">
+                  <li className="list-disc p-2 ">
+                    Create and Operate Document and MySQL Databases
+                  </li>
+                  <li className="list-disc p-2 ">
+                    Authentication and Authorization
+                  </li>
+                  <li className="list-disc p-2 ">MERN Stack</li>
+                </ul>
+              </div>
+              <p className="sm:w-[80%] w-full text-center text-lg p-8 font-semibold">
+                With ever changing industry landscape, I've grown to understand
+                and embrace the need to stay in the know to keep with it's pace.
+              </p>
+            </>
           ) : (
             <></>
           )}
