8813927c906ec90a8962b29c660139e7709270b1
str4ng3love <normalemailadress@gmail.com>
2024-03-19 08:18:46 +0100

    chore: add prettier related configs

--- app/snippets/page.tsx
+++ app/snippets/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
   return (
     <section className="min-h-reduced">
-      <div className="pt-20 flex justify-center flex-col">
+      <div className="m-20 mt-20 flex flex-col justify-center pt-20">
         <h2 className="">Mike's Code Snippets</h2>
       </div>
     </section>
